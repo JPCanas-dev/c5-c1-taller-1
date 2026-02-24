6c69859fc77b541d4d78b53ac2882e8d45795b22
Juan Cañas <juanjimenez025@gmail.com>
2026-02-24 09:53:39 -0500

    carga inicial del archivo

---
+++ c5-c1-taller-1/ejerc1_Andres_Juan.py
@@ -0,0 +1,22 @@
+
+cantp = int(input("¿Cuántas personas desea registrar?: "))
+
+for i in range(cantp):
+    print("Participante #",i+1)
+    nombre = input("Ingrese su nombre: ")
+    edadbuena = 0
+    while edadbuena == 0:
+        edad = int(input("Ingrese su edad: "))
+        if (edad > 0):
+            edadbuena = 1
+        else:
+            print("Ingrese una edad válida")
+    conocimiento = input("¿Tiene conocimientos básicos de computación? (si/no): ")
+    if (edad >= 15) and (conocimiento == "si"):
+        print("Puede participar en el taller")
+    else:
+        print("No cumple los requisitos")
+print("Proceso finalizado")
+
+
+
